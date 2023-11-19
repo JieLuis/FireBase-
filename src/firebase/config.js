f7c09ebb3f02b67cd7b3cb148b37dd99952afffd
JieLuis <104972485+JieLuis@users.noreply.github.com>
2023-11-19 14:00:44 +0800

    Project setup

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDc8Ct3k_Zl-r_NxwgTW2PFQqCnvh1_Y1g",
+  authDomain: "my-blog-f81a9.firebaseapp.com",
+  projectId: "my-blog-f81a9",
+  storageBucket: "my-blog-f81a9.appspot.com",
+  messagingSenderId: "388734870497",
+  appId: "1:388734870497:web:7da937bd356821f48ea8a0",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirebase = firebase.firestore();
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Home</div>
+  <p>My name is ile</p>
+  <button @click="handleClick">Here</button>
+  <input type="text" v-model="search" />
+  <p>search term - {{ search }}</p>
+  <div v-for="name in names" :key="name">{{ name }}</div>
+</template>
+
+<script>
+import { ref, watch, watchEffect } from "vue";
+export default {
+  setup() {
+    const names = ["mario", "Sevval", "Can"];
+    const search = ref(""); 
+    const handleClick = () => {};
+
+    watch(search, () => {
+      console.log("fired");
+    });
+
+    watchEffect(() => (console.log("hi"), search.value));
+
+    return { names, search };
+  },
+};
+</script>
