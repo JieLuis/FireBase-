c33cbd1d716e366ff49c86566d89d119ec74514a
JieLuis <104972485+JieLuis@users.noreply.github.com>
2023-11-19 14:41:22 +0800

    Create navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,27 @@
 <template>
-  <div></div>
+  <header>
+    <a href="/index.html"
+      ><img class="icon" src="../../images/icon.png" alt=""
+    /></a>
+
+    <h1 class="headbar-title">Thinkaboutmoonlight</h1>
+
+    <nav class="collapisble collapisble--expanded">
+      <img class="icon icon--right" src="/images/icon-right.svg" alt="" />
+      <ul class="collapisble__content">
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <div class="language-menu">
+          <div class="selected-language">English</div>
+          <ul class="language-list">
+            <li><a href="#" class="en">English</a></li>
+            <li><a href="/index-cn.html" class="cn">中文</a></li>
+          </ul>
+        </div>
+      </ul>
+    </nav>
+  </header>
 </template>
 <script>
 export default {};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import "./assets/mainMobie.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
